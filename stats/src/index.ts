14886beedc6933e5824863df37034487b954643e
sachetwasti1997 <sachetwasti61@gmail.com>
2023-09-16 21:48:01 +0530

    Reusable Logic To Reade data

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi There");
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MathResults';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+let manUnitedWin = 0;
+
+reader.data
+  .filter((cellMatch) => {
+    return (
+      (cellMatch[1] === 'Man United' && cellMatch[5] === MatchResult.HomeWin) ||
+      (cellMatch[2] === 'Man United' && cellMatch[5] === MatchResult.AwayWin)
+    );
+  })
+  .map((m) => {
+    console.log(m);
+    manUnitedWin++;
+  });
+
+console.log(manUnitedWin);
